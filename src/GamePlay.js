70de16e903273d193e1d4dbbd9a3974a3c368f7e
Lycano25 <102488078+Lycano25@users.noreply.github.com>
2022-06-06 18:34:56 -0400

    Add files via upload

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -7,22 +7,22 @@ GamePlayManager = {
     },
     preload: function() {
 		var me=this;
-        me.load.image('background',"assets/images/fondo1.png");
-		me.load.spritesheet("crow","assets/images/crow.png",97,120);
-		me.load.spritesheet("man","assets/images/wolve.png",156,187);
+        me.load.image('background',"assets/images/fon2.png");
+		me.load.spritesheet("crow","assets/images/xx.png",98.5,90);
+		me.load.spritesheet("man","assets/images/lui.png",193,190);
 		
     },
     create: function() {
 		var me=this;
         me.game.add.sprite(0, 0, 'background');
 		
-		me.crow= me.game.add.sprite(50,50,"crow");
-		me.crow.animations.add("right",[0,1,2,3],2,true);
-		me.crow.animations.add("left",[4,5,6,7],8,true);
+		me.crow= me.game.add.sprite(50,30,"crow");
+		me.crow.animations.add("right",[0,1,2,3,4],2,true);
+		
 		me.crow.animations.play("right");
 		
-		me.man=me.game.add.sprite(220,330,"man");
-		me.man.animations.add("right",[0,1,2,3,4,5],5,true);
+		me.man=me.game.add.sprite(220,460,"man");
+		me.man.animations.add("right",[0,1,2,3],4,true);
 		//me.man.animations.add("left",[15,14,13,12,11,10,9,8],10,true);
 		me.game.cursores=me.input.keyboard.createCursorKeys();
 		me.game.physics.startSystem(Phaser.Physics.ARCADE);
